d71a4fa6875a5c112ba56a016288068f54fb63b8
ogavvlik <gawlik.oliwia@gmail.com>
2026-01-22 17:11:39 +0100

    Dodanie dokomentacji do klasy Klient

--- wypozyczalnia/Klient.cs
+++ wypozyczalnia/Klient.cs
@@ -9,6 +9,10 @@ using System.Threading.Tasks;
 
 namespace WypozyczalniaNarciarska
 {
+    /// <summary>
+    /// Reprezentuje klienta wypożyczalni narciarskiej.
+    /// Dziedziczy po klasie Osoba i rozszerza ją o dane kontaktowe.
+    /// </summary>
     [DataContract]
     public class Klient : Osoba
     {
@@ -16,6 +20,12 @@ namespace WypozyczalniaNarciarska
         private string numerTelefonu = "000000000";
         [DataMember]
         private string email = "brak@brak.pl";
+
+        /// <summary>
+        /// Pobiera lub ustawia numer telefonu klienta.
+        /// Wyrzuca wyjątek kiedy numer telefonu jest pusty lub składa się tylko z białych znaków.
+        /// </summary>
+
         public string NumerTelefonu
         {
             get => numerTelefonu;
@@ -26,6 +36,11 @@ namespace WypozyczalniaNarciarska
                 numerTelefonu = value;
             }
         }
+
+        /// <summary>
+        /// Pobiera lub ustawia adres e-mail klienta.
+        /// Wyrzuca wyjątek kiedy nie wprowadzono znaku @.
+        /// </summary>
         public string Email
         {
             get => email;
@@ -36,13 +51,27 @@ namespace WypozyczalniaNarciarska
                 email = value;
             }
         }
+
+        /// <summary>
+        /// Tworzy nową instancję klienta z domyślnymi wartościami.
+        /// </summary>
+        /// 
         public Klient() : base() {}
 
+
+        /// <summary>
+        /// Tworzy nową instancję klienta z pełnymi danymi.
+        /// </summary>
+        /// 
         public Klient(string imie, string nazwisko, string pesel, DateTime dataUrodzenia, string numerTelefonu, string email): base(imie, nazwisko, pesel, dataUrodzenia)
         { 
             NumerTelefonu = numerTelefonu;
             Email = email;
         }
+
+        /// <summary>
+        /// Zwraca tekstowy opis klienta wraz z danymi kontaktowymi.
+        /// </summary>
         public override string Opis()
         {
             return base.Opis() +
