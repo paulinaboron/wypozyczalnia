70a999538535169192f7940ab0e8635d2843e095
ogavvlik <gawlik.oliwia@gmail.com>
2026-01-22 17:26:05 +0100

    DOdanie dokumentacji do klasy NiepoprawnaRezerwacjaException

--- wypozyczalnia/NiepoprawnaRezerwacjaException.cs
+++ wypozyczalnia/NiepoprawnaRezerwacjaException.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace WypozyczalniaNarciarska
 {
+    /// <summary>
+    /// Wyjątek zgłaszany w przypadku wykrycia niepoprawnych danych rezerwacji.
+    /// </summary>
     public class NiepoprawnaRezerwacjaException : Exception
     {
+        /// <summary>
+        /// Tworzy nowy wyjątek związany z błędną rezerwacją.
+        /// </summary>
         public NiepoprawnaRezerwacjaException(string message) : base(message) { }
     }
 }
