cd39e70b9c449e208614387378ce40f54b8858c0
ogavvlik <gawlik.oliwia@gmail.com>
2026-01-22 17:28:32 +0100

    Dodanie dokumentacji do klasy NiepoprawneDaneSprzetuException

--- wypozyczalnia/NiepoprawneDaneSprzetuException.cs
+++ wypozyczalnia/NiepoprawneDaneSprzetuException.cs
@@ -2,10 +2,24 @@ using System;
 
 namespace WypozyczalniaNarciarska
 {
+    /// <summary>
+    /// Wyj¹tek zg³aszany w przypadku podania niepoprawnych danych sprzêtu narciarskiego.
+    /// </summary>
     public class NiepoprawneDaneSprzetuException : ArgumentException
     {
+        /// <summary>
+        /// Tworzy wyj¹tek z domyœlnym komunikatem o niepoprawnych danych sprzêtu.
+        /// </summary>
         public NiepoprawneDaneSprzetuException() : base("Niepoprawne dane sprzêtu.") { }
+
+        /// <summary>
+        /// Tworzy wyj¹tek z podanym komunikatem opisuj¹cym b³¹d.
+        /// </summary>
         public NiepoprawneDaneSprzetuException(string message) : base(message) { }
+
+        /// <summary>
+        /// Tworzy wyj¹tek z podanym komunikatem oraz wyj¹tkiem wewnêtrznym.
+        /// </summary>
         public NiepoprawneDaneSprzetuException(string message, Exception inner) : base(message, inner) { }
     }
 }
